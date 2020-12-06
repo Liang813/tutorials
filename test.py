d86f9178dff73f5d299bbc90ceaef49cb01a2aca
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-12-06 16:29:09 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from tkinter import Variable
+
+from opt_einsum.backends import torch
+from tensorflow.python.ops import nn
+
+from beginner_source.blitz.cifar10_tutorial import net
+
+output = net(input)
+target = Variable(torch.arange(1, 11))  # a dummy target, for example
+criterion = nn.MSELoss()
+
+loss = criterion(output, target)
