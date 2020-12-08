225c68f57b1c253f33997a354cbc3f4b244dab91
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-12-08 21:12:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-from tkinter import Variable
-
 import torch
-from tensorflow.python.ops import nn
-
-from beginner_source.blitz.cifar10_tutorial import net
+from tkinter import Variable
+import nn
+from beginner_source.former_torchies.nn_tutorial import net
 
 output = net(input)
 target = Variable(torch.arange(1, 11))  # a dummy target, for example
