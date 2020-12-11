e316a3c08329335033a583f81af32e0b994ec439
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-12-11 08:53:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from tensorflow.python.ops import nn
 
 from beginner_source.former_torchies.nn_tutorial import net
 
-output = net((1))
+output = net((1,3))
 target = Variable(torch.arange(1, 11))  # a dummy target, for example
 criterion = nn.MSELoss()
 
